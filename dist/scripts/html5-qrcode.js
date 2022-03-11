16f3fab70ccbbaa59b10ee6ac9052668da9a49b4
donataskartanovicius <donatas.karta@gmail.com>
2022-03-11 21:31:04 +0200

    Ukraine Added

--- dist/scripts/html5-qrcode.js
+++ dist/scripts/html5-qrcode.js
@@ -16,43 +16,44 @@ function onScanSuccess(decodedText, decodedResult) {
 
     let decodedCountryCode = decodedText.substring(0, 3);
 
-    let countryCodesRu = [460,461,462,463,464,465,466,467,468,469]
-    let countryCodesUk = [482]
+    let countryCodesRussia = ["460", "461", "462", "463", "464", "465", "466", "467", "468", "469"]
+    let countryCodesUkraine = ["482"]
 
     if (decodedResult.result.format.formatName == "EAN_13") {
-        for (let i = 0; i < countryCodesRu.length; i++) {
-            if (decodedCountryCode == countryCodesRu[i].toString()) {
-                ukrainian.style.display = "none"
-                other.style.display = "none"
-                russian.style.display = "block"
-                break;
-            }
-            else if (decodedText == "3583788388838") {
-                other.style.display = "none"
-                russian.style.display = "none"
-                ukrainian.style.display = "block"
-                break;
-            } else {
-                ukrainian.style.display = "none"
-                russian.style.display = "none"
-                other.style.display = "block"
-                if (i = countryCodesRu.length) {
-                    break;
-                }
-            }
+
+        if (countryCodesRussia.includes(decodedCountryCode)) {
+            ukrainian.style.display = "none"
+            other.style.display = "none"
+            russian.style.display = "block"
+
+        } else if (countryCodesUkraine.includes(decodedCountryCode)) {
+            other.style.display = "none"
+            russian.style.display = "none"
+            ukrainian.style.display = "block"
+
+        } else {
+            ukrainian.style.display = "none"
+            russian.style.display = "none"
+            other.style.display = "block"
         }
     }
 
-  }
-  
-  function onScanFailure(error) {
+}
+
+function onScanFailure(error) {
     // handle scan failure, usually better to ignore and keep scanning.
     // for example:
     //console.warn(`Code scan error = ${error}`);
-  }
-  
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },
-    /* verbose= */ false);
-  html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader", {
+        fps: 10,
+        qrbox: {
+            width: 250,
+            height: 250
+        }
+    },
+    /* verbose= */
+    false);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
